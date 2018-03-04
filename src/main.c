612d7567352eaa9f84091e72de6c188116e11318
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2018-03-04 07:49:17 +0500

    feat(main): updated `main` file

--- src/main.c
+++ src/main.c
@@ -1,60 +1,22 @@
-#if 0
-#include "chunk.h"
+#include <stdio.h>
+
+#include "compiler.h"
 #include "debug.h"
-#include "value.h"
 #include "vm.h"
 
-/**
- * print 3 + 5
- */
-
 int main() {
+  const char *source = "print 1000 + 250 + 90 - 3";
+
   MAKE_VM(vm);
-  MAKE_CHUNK(chunk);
+  MAKE_COMPILER(compiler, source);
 
-  int varA = addConstant(&chunk, 3);
-  int varB = addConstant(&chunk, 5);
-
-  writeChunk(&chunk, OP_LOAD_CONSTANT);
-  writeChunk(&chunk, varA);
-  writeChunk(&chunk, OP_LOAD_CONSTANT);
-  writeChunk(&chunk, varB);
-  writeChunk(&chunk, OP_ADD);
-  writeChunk(&chunk, OP_PRINT);
+  Chunk chunk = compile(&compiler);
 
   interpret(&vm, &chunk);
   disassembleChunk(&chunk);
 
-  freeChunk(&chunk);
+  freeCompiler(&compiler);
   freeVM(&vm);
-
-  return 0;
-}
-#endif
-
-#include <stdio.h>
-
-#include "debug.h"
-#include "lexer.h"
-#include "token.h"
-
-int main() {
-  const char *source = "125 + 1 - 8     \n";
-
-  MAKE_LEXER(lexer, source);
-
-  Token token;
-  for (;;) {
-    token = lexToken(&lexer);
-
-    if (token.type == TOKEN_EOF) {
-      break;
-    }
-
-    printToken(&token);
-  }
-
-  freeLexer(&lexer);
 
   return 0;
 }
