95459505a9f48e1c1a2abed23972a57f7ad549aa
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2018-03-05 00:16:33 +0500

    feat(main): updated main CLI

--- src/main.c
+++ src/main.c
@@ -2,21 +2,47 @@
 
 #include "compiler.h"
 #include "debug.h"
+#include "util.h"
 #include "vm.h"
 
-int main() {
-  const char *source = "print 1000 + 400 - 80 + 20 - 3";
+int main(int argc, char **argv) {
+  const char *binname = argv[0];
+
+  if (argc != 2) {
+    printf("Usage:\n"
+           "  %s <filename>\n",
+           binname);
+
+    return 1;
+  }
+
+  char *filename = argv[1];
+  char *source = readFile(filename);
+
+  if (source == NULL) {
+    printf("File not found\n");
+    return 1;
+  }
+
+  printf("=== Source ===\n");
+  printf("%s\n", source);
+  printf("==============\n");
 
   MAKE_VM(vm);
   MAKE_COMPILER(compiler, source);
 
   Chunk chunk = compile(&compiler);
 
-  interpret(&vm, &chunk);
   disassembleChunk(&chunk);
+
+  printf("=== VM Results ===\n");
+  interpret(&vm, &chunk);
+  printf("==================\n");
 
   freeCompiler(&compiler);
   freeVM(&vm);
+
+  freeFile(source);
 
   return 0;
 }
