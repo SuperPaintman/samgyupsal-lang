25f78dfecfce466a824471a43e629bec8b0fbc60
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2018-03-04 07:47:09 +0500

    feat(compiler): added support for basic binary expressions

--- src/compiler.c
+++ src/compiler.c
@@ -2,7 +2,17 @@
 
 #include <stdlib.h>
 
+// TODO
+#include "debug.h"
 #include <stdio.h>
+
+// Declarations
+static void compileNumber(Compiler *compiler);
+static void compilePrintStatement(Compiler *compiler);
+static void maybeUnary(Compiler *compiler);
+static void maybeBinary(Compiler *compiler);
+static void maybeExpressionStatement(Compiler *compiler);
+static void maybeStatement(Compiler *compiler);
 
 VECTOR_TEMPLATE_IMPLEMENTATIONS(TokenVector, Token *);
 
@@ -40,6 +50,7 @@ static inline bool matchTokenType(Compiler *compiler, TokenType type) {
   return true;
 }
 
+// Emit
 static inline void emitCode(Compiler *compiler, OpCode code) {
   writeChunk(&compiler->chunk, code);
 }
@@ -51,24 +62,52 @@ static void emitLoadConstant(Compiler *compiler, Value value) {
   writeChunk(&compiler->chunk, val);
 }
 
+// Compile
 static void compileNumber(Compiler *compiler) {
   Value value = strtod(compiler->previous->start, NULL);
 
   return emitLoadConstant(compiler, value);
 }
 
-static void maybeExpressionStatement(Compiler *compiler) {
+static void compilePrintStatement(Compiler *compiler) {
+  maybeExpressionStatement(compiler);
+  emitCode(compiler, OP_PRINT);
+}
+
+// Maybe
+static void maybeUnary(Compiler *compiler) {
   if (matchTokenType(compiler, TOKEN_NUMBER)) {
     return compileNumber(compiler);
   }
 
-  printf("Unexpected Token\n");
+  printf("Unexpected Token:\n");
+  printToken(compiler->current);
   exit(1);
 }
 
-static void compilePrintStatement(Compiler *compiler) {
-  maybeExpressionStatement(compiler);
-  emitCode(compiler, OP_PRINT);
+static void maybeBinary(Compiler *compiler) {
+  maybeUnary(compiler);
+
+  if (matchTokenType(compiler, TOKEN_PLUS) ||
+      matchTokenType(compiler, TOKEN_MINUS)) {
+    TokenType opType = compiler->previous->type;
+
+    maybeExpressionStatement(compiler);
+
+    switch (opType) {
+    case TOKEN_PLUS:
+      return emitCode(compiler, OP_ADD);
+    case TOKEN_MINUS:
+      return emitCode(compiler, OP_SUB);
+    default:
+      // TODO: add error handler
+      exit(1);
+    }
+  }
+}
+
+static void maybeExpressionStatement(Compiler *compiler) {
+  return maybeBinary(compiler);
 }
 
 static void maybeStatement(Compiler *compiler) {
