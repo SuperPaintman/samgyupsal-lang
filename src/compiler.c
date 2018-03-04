5aca21ff5de2e4a3f2bcd19ff9ea44eda87b2683
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2018-03-04 23:55:53 +0500

    refactor(compiler): refactored compiler and fixed error with compiling of binary expressions

--- src/compiler.c
+++ src/compiler.c
@@ -7,15 +7,84 @@
 #include <stdio.h>
 
 // Declarations
-static void compileNumber(Compiler *compiler);
-static void compilePrintStatement(Compiler *compiler);
-static void maybeUnary(Compiler *compiler);
-static void maybeBinary(Compiler *compiler);
-static void maybeExpressionStatement(Compiler *compiler);
-static void maybeStatement(Compiler *compiler);
+/**
+ * ```ebnf
+ * program
+ *   = { stmt }
+ *   ;
+ * ```
+ */
+static void compileProgram(Compiler *compiler);
 
+/**
+ * ```ebnf
+ * stmt
+ *   = stmt_expr
+ *   | stmt_print
+ *   ;
+ * ```
+ */
+static void compileStmt(Compiler *compiler);
+
+/**
+ * ```ebnf
+ * stmt_print
+ *   = 'print' expr
+ *   ;
+ * ```
+ */
+static void compileStmtPrint(Compiler *compiler);
+
+/**
+ * ```ebnf
+ * stmt_expr
+ *   = expr
+ *   ;
+ * ```
+ */
+static void compileStmtExpr(Compiler *compiler);
+
+/**
+ * ```ebnf
+ * expr
+ *   = term
+ *   | expr_binary
+ *   ;
+ * ```
+ */
+static void compileExpr(Compiler *compiler);
+
+/**
+ * ```ebnf
+ * expr_binary
+ *   = term '+' term
+ *   | term '-' term
+ *   ;
+ * ```
+ */
+static void compileExprBinary(Compiler *compiler);
+
+/**
+ * ```ebnf
+ * term
+ *   = IDENTIFIER
+ *   | NUMBER
+ *   ;
+ * ```
+ */
+static void compileTerm(Compiler *compiler);
+
+/**
+ * ```ebnf
+ * NUMBER
+ * ```
+ */
+static void compileNumber(Compiler *compiler);
+
+// Vectors
 VECTOR_TEMPLATE_IMPLEMENTATIONS(TokenVector, Token *);
 
+// Helpers
 static inline bool isAtEnd(Compiler *compiler) {
   return compiler->current && compiler->current->type == TOKEN_EOF;
 }
@@ -50,7 +119,29 @@ static inline bool matchTokenType(Compiler *compiler, TokenType type) {
   return true;
 }
 
-// Emit
+// TOOD
+static void debugCompiler(Compiler *compiler) {
+  if (compiler->previous) {
+    printf("  previous:\n    ");
+    printToken(compiler->previous);
+  }
+  if (compiler->current) {
+    printf("  current:\n    ");
+    printToken(compiler->current);
+  }
+}
+
+// TOOD
+static void raiseError(Compiler *compiler) {
+  // TODO
+  printf("---------\n");
+  printf("Unexpected Token:\n");
+  debugCompiler(compiler);
+  printf("---------\n");
+  exit(1);
+}
+
+// Code Emitters
 static inline void emitCode(Compiler *compiler, OpCode code) {
   writeChunk(&compiler->chunk, code);
 }
@@ -63,61 +154,85 @@ static void emitLoadConstant(Compiler *compiler, Value value) {
 }
 
 // Compile
+static void compileProgram(Compiler *compiler) {
+  // Load the first token
+  next(compiler);
+
+  while (!matchTokenType(compiler, TOKEN_EOF)) {
+    compileStmt(compiler);
+  }
+}
+
+static void compileStmt(Compiler *compiler) {
+  // 'print'
+  if (matchTokenType(compiler, TOKEN_PRINT)) {
+    return compileStmtPrint(compiler);
+  }
+
+  return compileStmtExpr(compiler);
+}
+
+static void compileStmtPrint(Compiler *compiler) {
+  compileExpr(compiler);
+  emitCode(compiler, OP_PRINT);
+}
+
+static void compileStmtExpr(Compiler *compiler) {
+  return compileExpr(compiler);
+}
+
+static void compileExpr(Compiler *compiler) {
+  compileTerm(compiler);
+
+  compileExprBinary(compiler);
+}
+
+static void compileExprBinary(Compiler *compiler) {
+  if (isAtEnd(compiler)) {
+    return;
+  }
+
+  // TODO: add check for TOKEN_NEWLINE
+
+  if (!(matchTokenType(compiler, TOKEN_PLUS) ||
+        matchTokenType(compiler, TOKEN_MINUS))) {
+    raiseError(compiler);
+  }
+
+  TokenType opType = compiler->previous->type;
+
+  compileTerm(compiler);
+
+  switch (opType) {
+  case TOKEN_PLUS:
+    emitCode(compiler, OP_ADD);
+    break;
+  case TOKEN_MINUS:
+    emitCode(compiler, OP_SUB);
+    break;
+  default:
+    // TODO: add error handler
+    exit(1);
+  }
+
+  return compileExprBinary(compiler);
+}
+
+static void compileTerm(Compiler *compiler) {
+  if (matchTokenType(compiler, TOKEN_NUMBER)) {
+    return compileNumber(compiler);
+  }
+
+  raiseError(compiler);
+}
+
 static void compileNumber(Compiler *compiler) {
   Value value = strtod(compiler->previous->start, NULL);
 
   return emitLoadConstant(compiler, value);
 }
 
-static void compilePrintStatement(Compiler *compiler) {
-  maybeExpressionStatement(compiler);
-  emitCode(compiler, OP_PRINT);
-}
-
-// Maybe
-static void maybeUnary(Compiler *compiler) {
-  if (matchTokenType(compiler, TOKEN_NUMBER)) {
-    return compileNumber(compiler);
-  }
-
-  printf("Unexpected Token:\n");
-  printToken(compiler->current);
-  exit(1);
-}
-
-static void maybeBinary(Compiler *compiler) {
-  maybeUnary(compiler);
-
-  if (matchTokenType(compiler, TOKEN_PLUS) ||
-      matchTokenType(compiler, TOKEN_MINUS)) {
-    TokenType opType = compiler->previous->type;
-
-    maybeExpressionStatement(compiler);
-
-    switch (opType) {
-    case TOKEN_PLUS:
-      return emitCode(compiler, OP_ADD);
-    case TOKEN_MINUS:
-      return emitCode(compiler, OP_SUB);
-    default:
-      // TODO: add error handler
-      exit(1);
-    }
-  }
-}
-
-static void maybeExpressionStatement(Compiler *compiler) {
-  return maybeBinary(compiler);
-}
-
-static void maybeStatement(Compiler *compiler) {
-  if (matchTokenType(compiler, TOKEN_PRINT)) {
-    return compilePrintStatement(compiler);
-  }
-
-  return maybeExpressionStatement(compiler);
-}
-
+// Exposed
 void initCompiler(Compiler *compiler, const char *source) {
   MAKE_LEXER(lexer, source);
   MAKE_CHUNK(chunk);
@@ -138,12 +253,7 @@ void freeCompiler(Compiler *compiler) {
 }
 
 Chunk compile(Compiler *compiler) {
-  // Load the first token
-  next(compiler);
-
-  while (!matchTokenType(compiler, TOKEN_EOF)) {
-    maybeStatement(compiler);
-  }
+  compileProgram(compiler);
 
   return compiler->chunk;
 }
